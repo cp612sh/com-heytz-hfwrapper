76a494241dd004cd50bc834ad92702a46136e807
mc <michaelchai@heytz.com>
2015-12-02 15:07:59 +0800

    fix the export parameters

--- www/hfwrapper.js
+++ www/hfwrapper.js
@@ -1,5 +1,5 @@
-HFWrapper = {
-    start: function (appId, productKey, uid, userToken, success, failure) {
-        cordova.exec(success, failure, 'HFWrapper', 'start', [appId, productKey, uid, userToken, wifiSSID, wifiKey]);
-    },
+var exec = require('cordova/exec');
+
+exports.start = function (appId, productKey, uid, userToken, wifiSSID, wifiKey, success, failure) {
+    exec(success, failure, 'HFWrapper', 'start', [appId, productKey, uid, userToken, wifiSSID, wifiKey]);
 };
